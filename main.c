e600c25d8fda2d1b5a48aab24f99aacbbde35f3f
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-05-17 07:02:34 -0700

    my commit file

---
+++ main.c
@@ -0,0 +1,64 @@
+#include "shell.c"
+
+/**
+ * main - Function that uns a simple UNIX command interpreter
+ * @argc: number of arguments
+ * @argv: array of pointers to the arguments.
+ * Return: The return value of the last executed command.
+ */
+int main(int argc, char *argv[])
+{
+	int ret = 0, ret_end;
+	int *exit_ret = &ret_end;
+	char *prompt = "cisfun$ ", *new_line = "\n";
+
+	name = argv[0];
+	hist = 1;
+	aliases = NULL;
+	signal(SIGINT, sig_handler);
+
+	*exit_ret = 0;
+	environ_var = _copyenv();
+	if (!environ_var)
+	{
+		exit(-100);
+	}
+	if (argc != 1)
+	{
+		ret = proc_file_commands(argv[1], exit_ret);
+		free_env();
+		free_alias_list(aliases);
+		return (*exit_ret);
+	}
+
+	if (!isatty(STDIN_FILENO))
+	{
+		while (ret != END_OF_FILE && ret != EXIT)
+		{
+			ret = handle_args(exit_ret);
+		}
+		free_env();
+		free_alias_list(aliases);
+		return (*exe_ret);
+	}
+
+	while (1)
+	{
+		write(STDOUT_FILENO, prompt, 2);
+		ret = handle_args(exit_ret);
+		if (ret == END_OF_FILE || ret == EXIT)
+		{
+			if (ret == END_OF_FILE)
+			{
+				write(STDOUT_FILENO, new_line, 1);
+			}
+			free_env();
+			free_alias_list(aliases);
+			exit(*exit_ret);
+		}
+	}
+
+	free_env();
+	free_alias_list(aliases);
+	return (*exit_ret);
+}
---
+++ shell.h
@@ -0,0 +1,20 @@
+#ifndef _SHELL_H
+#define _SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define END_OF_FILE -2
+#define EXIT -3
+
+
+
+
+#endif
