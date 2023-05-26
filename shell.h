3b4d9dceacfa03461f8bdbbc5d9c5994e198aac0
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-05-26 16:40:06 -0700

    my commit file

--- shell.h
+++ shell.h
@@ -14,11 +14,9 @@
 #define END_OF_FILE -2
 #define EXIT -3
 
-/* Global environemnt */
+
 extern char **environ;
-/* Global program name */
 char *name;
-/* Global history counter */
 int hist;
 
 /**
@@ -56,10 +54,8 @@ typedef struct alias_s
 	struct alias_s *next;
 } alias_t;
 
-/* Global aliases linked list */
 alias_t *aliases;
 
-/* Main Helpers */
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char **_strtok(char *line, char *delim);
@@ -69,7 +65,6 @@ int execute(char **args, char **front);
 void free_list(list_t *head);
 char *_itoa(int num);
 
-/* Input Helpers */
 void handle_line(char **line, ssize_t read);
 void variable_replacement(char **args, int *exe_ret);
 char *get_args(char *line, int *exe_ret);
@@ -80,7 +75,6 @@ int check_args(char **args);
 void free_args(char **args, char **front);
 char **replace_aliases(char **args);
 
-/* String functions */
 int _strlen(const char *s);
 char *_strcat(char *dest, const char *src);
 char *_strncat(char *dest, const char *src, size_t n);
@@ -90,7 +84,6 @@ int _strspn(char *s, char *accept);
 int _strcmp(char *s1, char *s2);
 int _strncmp(const char *s1, const char *s2, size_t n);
 
-/* Builtins */
 int (*get_builtin(char *command))(char **args, char **front);
 int shellby_exit(char **args, char **front);
 int shellby_env(char **args, char __attribute__((__unused__)) **front);
@@ -100,12 +93,10 @@ int shellby_cd(char **args, char __attribute__((__unused__)) **front);
 int shellby_alias(char **args, char __attribute__((__unused__)) **front);
 int shellby_help(char **args, char __attribute__((__unused__)) **front);
 
-/* Builtin Helpers */
 char **_copyenv(void);
 void free_env(void);
 char **_getenv(const char *var);
 
-/* Error Handling */
 int create_error(char **args, int err);
 char *error_env(char **args);
 char *error_1(char **args);
@@ -115,7 +106,6 @@ char *error_2_syntax(char **args);
 char *error_126(char **args);
 char *error_127(char **args);
 
-/* Linkedlist Helpers */
 alias_t *add_alias_end(alias_t **head, char *name, char *value);
 void free_alias_list(alias_t *head);
 list_t *add_node_end(list_t **head, char *dir);
